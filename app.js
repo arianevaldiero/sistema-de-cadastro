607e0abd1f8048b55756e5786db3b7b4019eba61
Ariane Valdiero <arianevaldiero@hotmail.com>
2023-09-07 18:47:01 -0300

    voltando para etapa anterior

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando sistem de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
-console.log("O Paulo fez esse commit sem voce ver na aula!");
-console.log("Enviando para o GitHub");
